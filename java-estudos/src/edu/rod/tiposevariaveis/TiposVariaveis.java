1f0989613756290a020bd5672407074a0414f7c7
siqueirarxd <rsiqueira958@gmail.com>
2023-05-14 20:38:05 -0300

    commit3

--- java-estudos/src/edu/rod/tiposevariaveis/TiposVariaveis.java
+++ java-estudos/src/edu/rod/tiposevariaveis/TiposVariaveis.java
@@ -4,9 +4,16 @@ public class TiposVariaveis {
     public static void main(String[] args) {
         double salarioMinimo = 2500.00; //uso mais comum de casas flutuantes
         float troco = 25.50F; //float só funciona com o F no final.
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
 
+        
         final int ANO = 2023; //constante.
         final double VALOR_DE_PI = 3.14; //constante.
-
+        
     }
 }
