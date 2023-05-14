2688a3cad1bdc9b4fb827710ea6a10bd3e7b097d
siqueirarxd <rsiqueira958@gmail.com>
2023-05-14 20:27:18 -0300

    tipos de variaveis

---
+++ java-estudos/src/edu/rod/tiposevariaveis/TiposVariaveis.java
@@ -0,0 +1,12 @@
+package edu.rod.tiposevariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.00; //uso mais comum de casas flutuantes
+        float troco = 25.50F; //float só funciona com o F no final.
+
+        final int ANO = 2023; //constante.
+        final double VALOR_DE_PI = 3.14; //constante.
+
+    }
+}
